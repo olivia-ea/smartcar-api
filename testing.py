557fabda56682b7098ab35f916e70b71a5da83eb
Olivia Ea <ea.olivia1@gmail.com>
2019-08-15 17:21:49 -0700

    Validation tests added

--- testing.py
+++ testing.py
@@ -1,46 +1,156 @@
 from smartcar_api import *
-from gm_api import *
 import unittest
 from unittest.mock import Mock, patch
-
-
 from nose.tools import assert_true, assert_list_equal, assert_is_not_none
 import requests
 
+from smartcar_api import app
 
-# Response.ok
-# (Bool) True if no errors occurred during the request, and the status_code is kosher.
-# If you invoke .raise_for_status(), an HTTPError will be raised for certain status codes. If the status code indicates a successful request, the program will proceed without that exception being raised.
-
+# PRIOR TO RUNNING testing.py: python3 server.py 
 # RUN IN TERMINAL: nosetests --verbosity=2 testing.py
 
-# class Test_GM_Status_Code(unittest.TestCase):
 
-#     GM_API_URL = "http://gmapi.azurewebsites.net"
+# TODO: do json validation to make sure the output is correct, put coverage % in README
+# Example: battery sometimes gives null so test for that edge case
+# To look up: how to test post?
 
-#     GM_endpoints = ["getVehicleInfoService", "getSecurityStatusService", "getEnergyService", "actionEngineService"]
+
+SMARTCAR_API = "http://localhost:5000/vehicles"
+INVALID_ID = f"{SMARTCAR_API}/0000"
+VALID_ID = f"{SMARTCAR_API}/1234"
+
+class TestSmartcarAPI(unittest.TestCase):
+
+    def test_valid_vehicle_info(self):
+
+        response = requests.get(f"{VALID_ID}")
+
+        try:
+            assert_true(response.status_code == 200)
+            assert_is_not_none(response)
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+    def test_invalid_vehicle_info(self):
+
+       response = requests.get(f"{INVALID_ID}")
+
+        try:
+            assert_true(response.status_code == 404)
+            assert_is_not_none(response)    # assert is none? or just error code?
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
 
 
-#     def test_status_code(self):
+    def test_valid_vehicle_doors():
 
-#         response = requests.get(f"{GM_API_URL}/getSecurityStatusService")
-        
+            response = requests.get(f"{VALID_ID/doors}")
 
-#         try:
-#             assert_true(response.status_code == 200)
-#             response.raise_for_status()
+        try:
+            assert_true(response.status_code == 200)
+            assert_is_not_none(response)
+            response.raise_for_status()
 
-#         except HTTPError as http_err:
-#             print(f'HTTP error occurred: {http_err}') 
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
 
-#     def test_request_response(self):
 
-#         response = requests.get(f"{GM_API_URL}/getSecurityStatusService")
+    def test_invalid_vehicle_doors():
 
-#         try:
-#             assert_is_not_none(response)
-#         except:
-#             print("No response.")
+       response = requests.get(f"{INVALID_ID}")
+
+        try:
+            assert_true(response.status_code == 404)
+            assert_is_not_none(response)    # assert is none? or just error code?
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+    def test_valid_vehicle_fuel():
+
+        response = requests.get(f"{VALID_ID/fuel}")
+
+        try:
+            assert_true(response.status_code == 200)
+            assert_is_not_none(response)
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+
+    def test_invalid_vehicle_fuel():
+
+
+       response = requests.get(f"{INVALID_ID}/fuel")
+
+        try:
+            assert_true(response.status_code == 404)
+            assert_is_not_none(response)    # assert is none? or just error code?
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+    def test_valid_vehicle_battery():
+
+        response = requests.get(f"{VALID_ID/battery}")
+
+        try:
+            assert_true(response.status_code == 200)
+            assert_is_not_none(response)
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+
+    def test_invalid_vehicle_battery():
+
+
+       response = requests.get(f"{INVALID_ID}/battery")
+
+        try:
+            assert_true(response.status_code == 404)
+            assert_is_not_none(response)    # assert is none? or just error code?
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+
+    def test_valid_vehicle_engine():
+
+        response = requests.get(f"{VALID_ID/engine}")
+
+        try:
+            assert_true(response.status_code == 200)
+            assert_is_not_none(response)
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+
+    def test_invalid_vehicle_engine():
+
+
+       response = requests.get(f"{INVALID_ID}/engine")
+
+        try:
+            assert_true(response.status_code == 404)
+            assert_is_not_none(response)    # assert is none? or just error code?
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}') 
+
+
 
 # @patch('gm_api.requests.get')
 # def test_status_code(mock_get):
@@ -52,29 +162,29 @@ import requests
 #     assert_is_not_none(response)
 
 
-class TestGMAPIs(object):
-    @classmethod
-    def setup_class(cls):
-        cls.mock_get_patcher = patch('gm_api.requests.get')
-        cls.mock_get = cls.mock_get_patcher.start()
+# class TestGMAPIs(object):
+#     @classmethod
+#     def setup_class(cls):
+#         cls.mock_get_patcher = patch('gm_api.requests.get')
+#         cls.mock_get = cls.mock_get_patcher.start()
 
-    @classmethod
-    def teardown_class(cls):
-        cls.mock_get_patcher.stop()
+#     @classmethod
+#     def teardown_class(cls):
+#         cls.mock_get_patcher.stop()
 
-    def test_GM_status_code(self):
+#     def test_GM_status_code(self):
 
-        self.mock_get.return_value.ok = True
+#         self.mock_get.return_value.ok = True
 
-        self.mock_get.return_value = Mock()
-        
-        self.mock_get.return_value.json.return_value = data
+#         self.mock_get.return_value = Mock()
 
-        # Call the service, which will send a request to the server.
-        response = gm_get_vehicle_info('1234')
+#         self.mock_get.return_value.json.return_value = data
 
-        # If the request is sent successfully, then I expect a response to be returned.
-        assert_equal(response.json(), data)
+#         # Call the service, which will send a request to the server.
+#         response = gm_get_vehicle_info('1234')
+
+#         # If the request is sent successfully, then I expect a response to be returned.
+#         assert_is_not_none(response)
 
     # def test_getting_todos_when_response_is_not_ok(self):
     #     # Configure the mock to not return a response with an OK status code.
@@ -85,6 +195,67 @@ class TestGMAPIs(object):
 
     #     # If the response contains an error, I should get no todos.
     #     assert_is_none(response)
+
+
+
+
+# class TestSmartcarAPI(unittest.TestCase):
+
+#     # def setUp(self):
+#     #     self.backup_items = deepcopy(app.items)  # no references!
+#     #     self.app = app.app.test_client()
+#     #     self.app.testing = True
+
+#     def test_get_all(self):
+#         response = self.app.get(BASE_URL)
+#         data = json.loads(response.get_data())
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(len(data['items']), 3)
+
+#     def test_get_one(self):
+#         response = self.app.get(BASE_URL)
+#         data = json.loads(response.get_data())
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(data['items'][0]['name'], 'laptop')
+
+#     def test_item_not_exist(self):
+#         response = self.app.get(BAD_ITEM_URL)
+#         self.assertEqual(response.status_code, 404)
+
+#     def test_post(self):
+#         # missing value field = bad
+#         item = {"name": "some_item"}
+#         response = self.app.post(BASE_URL,
+#                                  data=json.dumps(item),
+#                                  content_type='application/json')
+#         self.assertEqual(response.status_code, 400)
+#         # value field cannot take str
+#         item = {"name": "screen", "value": 'string'}
+#         response = self.app.post(BASE_URL,
+#                                  data=json.dumps(item),
+#                                  content_type='application/json')
+#         self.assertEqual(response.status_code, 400)
+#         # valid: both required fields, value takes int
+#         item = {"name": "screen", "value": 200}
+#         response = self.app.post(BASE_URL,
+#                                  data=json.dumps(item),
+#                                  content_type='application/json')
+#         self.assertEqual(response.status_code, 201)
+#         data = json.loads(response.get_data())
+#         self.assertEqual(data['item']['id'], 4)
+#         self.assertEqual(data['item']['name'], 'screen')
+#         # cannot add item with same name again
+#         item = {"name": "screen", "value": 200}
+#         response = self.app.post(BASE_URL,
+#                                  data=json.dumps(item),
+#                                  content_type='application/json')
+#         self.assertEqual(response.status_code, 400)
+
+   
+#     def tearDown(self):
+#         # reset app.items to initial state
+#         app.items = self.backup_items
+
 
 
 
